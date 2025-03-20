f334ffbc2ed15686cc5e7cc98a3a399182252ed4
Rieley <114270481+rieleyhunt@users.noreply.github.com>
2025-03-20 02:05:54 +0000

    Updated BolusScreen.h

--- BolusScreen.h
+++ BolusScreen.h
@@ -2,17 +2,26 @@
 #ifndef BOLUSSCREEN_H
 #define BOLUSSCREEN_H
 
-#include <QWidget>
 
-class BolusScreen : public QWidget {
+#include <QObject>
+#include "BolusCalculatorView.h"
+
+class BolusScreen : public QObject {
     Q_OBJECT
+
 public:
-    explicit BolusScreen(QWidget *parent = nullptr);
-    ~BolusScreen();
+    explicit BolusScreen(BolusCalculatorView& view, QObject* parent = nullptr);
+
+signals:
+    void bolusConfirmed(double dose); //Final confirmation signal
 
 private slots:
     void enterBolusAmount();
     void calculateBolus();
+    void confirmAndDeliverBolus();
+
+private:
+    BolusCalculatorView& view;
 };
 
-#endif // BOLUSSCREEN_H
+#endif // BOLUS_SCREEN_H
